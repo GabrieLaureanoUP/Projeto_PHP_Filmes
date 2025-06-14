2f377787e7402d8c829b2667c29c7d03a7edab2f
Marcos Maues <yboostx@gmail.com>
2025-06-14 15:55:17 -0300

    model comentario

--- Model/Comentario.php
+++ Model/Comentario.php
@@ -4,11 +4,11 @@ require_once __DIR__ . "/../Config/BancoPdo.php";
 
 class Comentario{
     public static function salvarComentario($filme_id, $usuario_id, $comentario){
-        $sql = "SELECT id FROM comentarios WHERE filme_id = :filme_id AND usuario_id = usuario_id";
+        $sql = "SELECT id FROM comentarios WHERE filme_id = :filme_id AND usuario_id = :usuario_id";
 
         $stmt = Database::conectar()->prepare($sql);
         $stmt->execute([
-          'filmes_id' => $filme_id,
+          'filme_id' => $filme_id,
           'usuario_id' => $usuario_id,
         ]);
 
@@ -26,7 +26,7 @@ class Comentario{
         ]);
     }
 
-    
+
 }
 
 ?>
