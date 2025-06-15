5627995dada0353e303eaf4f0f6ee6dccbf0b3bf
Marcos Maues <yboostx@gmail.com>
2025-06-15 15:02:40 -0300

    implementação de visualização de comentarios

--- View/filmes/comentar.php
+++ View/filmes/comentar.php
@@ -20,4 +20,23 @@ include __DIR__ . '/../partials/header.php';
     <button type="submit">Enviar Comentário</button>
 </form>
 
+<?php
+    require_once __DIR__ . "/../Config/BancoPdo.php";
+    $sql = "SELECT usuario, comentario, data FROM comentarios WHERE filme_id = :filme_id ORDER BY data DESC";
+    $stmt = Database::conectar()->prepare($sql);
+    $stmt->execute(['filme_id' => $filme_id]);
+    $comentarios = $stmt->fetchAll();
+    
+    if($comentarios){
+    foreach ($comentarios as $comentario) {
+        echo "<div class='comentario'>";
+        echo "<strong>" . htmlspecialchars($comentario['usuario']) . "</strong>: " . htmlspecialchars($comentario['comentario']);
+        echo "<br><small>" . date("d/m/Y H:i", strtotime($comentario['data'])) . "</small>";
+        echo "</div>";
+    }}else{
+        echo "<strong>Nenhum comentário feito!</strong>";
+    }
+?>
+
+
 <?php include __DIR__ . '/../partials/footer.php'; ?>
